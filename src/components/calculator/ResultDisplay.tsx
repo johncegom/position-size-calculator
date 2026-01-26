7ec6b6a3314a2bb09ff3c3856513a805cfb97da1
Minh Duong <johncegom203@gmail.com>
2026-01-27 03:16:48 +0700

    fix: redudant ResultItem component

--- src/components/calculator/ResultDisplay.tsx
+++ src/components/calculator/ResultDisplay.tsx
@@ -3,24 +3,6 @@ import type { RootState } from "../../store/store";
 import { formatToTwoDecimals } from "../../utils/formatters";
 import { useTranslation } from "react-i18next";
 
-type ResultItemProps = {
-  label: string;
-  value: number;
-};
-
-const ResultItem = ({ label, value }: ResultItemProps) => {
-  return (
-    <div className="p-4 mt-4 border border-gray-200 rounded-md dark:border-gray-400 bg-gray-50 dark:bg-gray-800">
-      <p className="text-gray-800 dark:text-gray-50">
-        <span className="font-medium cursor-default">{label}: </span>{" "}
-        <span className="font-bold text-blue-600 dark:text-blue-400">
-          ${formatToTwoDecimals(value)}
-        </span>
-      </p>
-    </div>
-  );
-};
-
 const ResultDisplay = () => {
   const { calculationResult } = useSelector(
     (state: RootState) => state.calculator,
